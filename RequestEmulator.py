6b924285747c6f4929e4f04c99f65f690ad0ca58
Cameron Taylor <cetaylor758903@knights.ucf.edu>
2018-09-05 19:47:23 -0400

    Reorganized to better integrate with C#

--- RequestEmulator.py
+++ RequestEmulator.py
@@ -8,20 +8,23 @@ class RequestEmulator:
     locations = ["Orlando City Hall", "Harvard University", "Stanford University"]
     address = ["111 Downtown Road, Orlando FL, 32881", "111 Harvard Road, Boston MA, 48376", "1844 Stanford Road, Stanford CA, 21987"]
     locationInfo = {}
-    
+
     def __init__(self):
-        self.locationInfo["name"] = self.locations[np.random.randint(0,high=2)]
-        self.locationInfo["address"] = self.address[np.random.randint(0,high=2)]
-        self.callTimelineEngine()
+        self.TimelineEngine = TE.TimelineEngine()
 
     def callTimelineEngine(self):
-        TE.TimelineGenerationCheck(self.locationInfo)
+        self.generateFakeLocation()
+        self.TimelineEngine.getWikiPage(self.locationInfo)
 
-def main():
-
-    RequestEmulator()
-    while False:
-        RequestEmulator()
+    def generateFakeLocation(self):
+        self.locationInfo["name"] = self.locations[np.random.randint(0,high=2)]
+        self.locationInfo["address"] = self.address[np.random.randint(0,high=2)]
+    
+    
+def main(): 
+    RE = RequestEmulator()  
+    while True:
+        RE.callTimelineEngine()
         time.sleep(np.random.randint(10,high=20))
 
 main()
--- TimelineEngine.py
+++ TimelineEngine.py
@@ -1,33 +1,20 @@
 import tensorflow as tf
 import urllib3 as url
-from bs4 import BeautifulSoup
-import requests
+import wikipedia
 import re
 
-def createGoogleSearch(locationInfo):
-    searchQuery = "\"history\" {}".format(locationInfo['name'])
-    print(searchQuery)
+class TimelineEngine():
 
-    return searchQuery
+    def __init__(self):
+        self.temp = 1
 
-def getURLs(searchQuery):
-    page = requests.get("https://www.google.com/search?q={}".format(searchQuery))
-    soup = BeautifulSoup(page.content)
-
-    links = []
-    for link in soup.find_all("a",href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-        print(re.split(":(?=http)",link["href"].replace("/url?q=","")))
-        links.append(link)
-    
-
-    return links
-
-def TimelineGenerationCheck(locationInfo):
-
-    URLs = getURLs(createGoogleSearch(locationInfo))
-
-    return True
+    def getWikiPage(self, locationInfo):
+        pages = wikipedia.search("{}".format(locationInfo["address"]))
+        print(pages)
 
 
+    def TimelineGenerationCheck(self, locationInfo):
+
+        return True
 
 
