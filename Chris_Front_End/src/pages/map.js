1e6d19d0b9d318c9d82d2fdc6a53640c207c454b
Christopher Louis Taliaferro <guitartaliaferro@knights.ucf.edu>
2018-09-17 23:02:33 -0400

    commit

--- Chris_Front_End/src/pages/map.js
+++ Chris_Front_End/src/pages/map.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StyleSheet, View} from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class Map extends Component {
     render() {
         return(
             <View style={styles.container}>
-                <Text>Map</Text>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 28.6014,
+                        longitude: -81.1987,
+                        latitudeDelta: 0.0030,
+                        longitudeDelta: 0.0030,}}>
+                    <MapView.Marker
+                        coordinate={{
+                            latitude: 28.6014,
+                            longitude: -81.1987,
+                        }}>
+                        <View style={styles.radius}>
+                            <View style={styles.marker}/>
+                        </View>
+                    </MapView.Marker>
+                </MapView>
             </View>
         )
     }
@@ -13,9 +30,39 @@ export default class Map extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      flexGrow: 1,
-      backgroundColor: '#2e88ff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flexGrow: 1,
+        backgroundColor: '#2e88ff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
+
+    map: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        position: 'absolute'
+    },
+
+    radius:{
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        overflow: 'hidden',
+        backgroundColor: 'rgba(0, 122, 255, 0.1)',
+        borderWidth: 1,
+        borderColor: 'rgba(0, 122, 255, 0.3)',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    marker: {
+        height: 20,
+        width: 20,
+        borderWidth: 3,
+        borderColor: 'white',
+        borderRadius: 10,
+        overflow: 'hidden',
+        backgroundColor: '#007AFF'
+    }
 });
