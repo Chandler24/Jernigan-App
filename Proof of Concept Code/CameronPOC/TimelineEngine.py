a188c3031423817c41f93aa220ae0be4c373704c
Cameron Taylor <cetaylor758903@knights.ucf.edu>
2018-10-03 21:22:40 -0400

    Added TCP server and fixed up timeline engine to work with it. JSON query in the form curl -XGET '<server>:<port>' -d '<LocationName>' should return json object with generated timeline.

--- Proof of Concept Code/CameronPOC/TimelineEngine.py
+++ Proof of Concept Code/CameronPOC/TimelineEngine.py
@@ -2,19 +2,18 @@ import wikipedia
 import re
 from gensim.summarization.summarizer import summarize
 import sys
-
-class TimelineObject():
-    pass
+import json
 
 class TimelineEngine():
 
     def getWikiPage(self, locationInfo):
-        result = wikipedia.search("{}".format(locationInfo["name"]))[0]
+        result = wikipedia.search("{}".format(locationInfo))[0]
         page = wikipedia.page(result)
         return page
 
 
     def getPageContent(self, locationInfo):
+        print(locationInfo)
         page = self.getWikiPage(locationInfo)
 
         page_history = page.section("History")
@@ -32,7 +31,6 @@ class TimelineEngine():
         return images[0]
     
     def generateTimeline(self, locationInfo):
-        print(locationInfo['name'])
         summary_text, ratio, images = self.getPageContent(locationInfo)
         #summary_text = re.sub('[^A-Za-z0-9.]+', ' ', summary_text).lstrip()
         summary_text = summarize(summary_text, ratio=ratio, split=True)
@@ -43,52 +41,25 @@ class TimelineEngine():
             if bool(re.search('[1-4][0-9]{3}', sentence)) and bool(re.search('in', sentence)):
                 timeline_sentences.append(sentence)
 
+        TimelineObject = {}
+        images = 
         
-        
-        image = self.findBestImage(images, locationInfo['name'])
+        image = self.findBestImage(images, locationInfo)
 
-        TEObject = TimelineObject()
-        TEObject.timeline_sentences = timeline_sentences[0:4]
-        TEObject.image = image
+        years_sentences = {}
 
-        return TEObject
+        for sentence in timeline_sentences:
+            years_sentences[re.search('[1-4][0-9]{3}', sentence).group()] = sentence
+
+        TimelineObject['sentences'] = years_sentences
+        TimelineObject['image'] = image
+
+        print(TimelineObject)
+        return json.dumps(TimelineObject)
             
 
     def TimelineGenerationCheck(self, locationInfo):
-        if bool(re.search(locationInfo['name'], self.getPageContent(locationInfo)[0])):         
+        if bool(re.search(locationInfo, self.getPageContent(locationInfo)[0])):         
             return True
         else:
             return True
-
-
-def main():
-    if len(sys.argv) < 2:
-        locationInfo={}
-        locationInfo['name'] = "Harvard University"
-        locationInfo['address'] = "123 Fake Address Lane"
-        TE = TimelineEngine()
-        TEObject = TE.generateTimeline(locationInfo)
-
-        f = open("output.txt", "w")
-        for line in TEObject.timeline_sentences:
-            f.write(line)
-
-        f.write("\n\n")
-        f.write(TEObject.image)
-    else:
-        locationInfo={}
-        locationInfo['name'] = sys.argv[1]
-        locationInfo['address'] = sys.argv[2]
-        TE = TimelineEngine()
-        TEObject = TE.generateTimeline(locationInfo)
-
-        f = open("output.txt", "w")
-        for line in TEObject.timeline_sentences:
-            f.write(line)
-
-        f.write("\n\n")
-        f.write(TEObject.image)
-
-
-if __name__ == "__main__":
-    main()
