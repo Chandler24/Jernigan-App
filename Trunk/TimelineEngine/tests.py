30c4b6183f32483f62a50d8756f9f996497dc91e
Cameron Taylor <cetaylor758903@knights.ucf.edu>
2018-10-07 00:30:10 -0400

    Accidently forgot to commit changes to make tests work

--- Trunk/TimelineEngine/tests.py
+++ Trunk/TimelineEngine/tests.py
@@ -5,40 +5,38 @@ import unittest
 import wikipedia
 
 class UnitTests(unittest.TestCase):
-
-    def __init__(self):
-        self.TE = TimelineEngine.TimelineEngine()
-        self.page = wikipedia.page("Stanford University")
-        self.images = self.page.images
-        self.page_content = (self.page, .1, self.images)
-        os.system("python server.py")
+    TE = TimelineEngine.TimelineEngine()
+    page = wikipedia.page("Stanford University")
+    images = page.images
+    page_content = (page, .1, images)
 
     def test_getWikiPage(self):
-        self.assertIs(self.TE.getWikiPage("Stanford University"), str)
-        self.assertIs(self.TE.getWikiPage(""), None)
-        self.assertIs(self.TE.getWikiPage(None), None)
+        self.assertIsInstance(self.TE.getWikiPage("Stanford University"), type(self.page))
+        self.assertEqual(self.TE.getWikiPage(""), None)
+        self.assertEqual(self.TE.getWikiPage(None), None)
 
     def test_getPageContent(self):
-        self.assertIs(self.TE.getWikiPage("Stanford University"), type(self.page_content))
-        self.assertIs(self.TE.getWikiPage(""), None)
-        self.assertIs(self.TE.getWikiPage(None), None)
+        self.assertIsInstance(self.TE.getWikiPage("Stanford University"), type(self.page))
+        self.assertEqual(self.TE.getWikiPage(""), None)
+        self.assertEqual(self.TE.getWikiPage(None), None)
         
     def test_findBestImage(self):
-        self.assertEqual(self.TE.findBestImage(self.images, "Stanford University"), typeof(self.images[0]))
+        self.assertIsInstance(self.TE.findBestImage(self.images, "Stanford University"), type(self.images[0]))
         self.assertEqual(self.TE.findBestImage([], "Stanford University"), None)
         self.assertEqual(self.TE.findBestImage(None, ""), None)
         
     def test_generateTimeline(self):
-        self.assertIs(self.TE.getWikiPage("Stanford University"), str)
-        self.assertEqual(self.TE.getWikiPage(""), "No Timeline Avaliable")
-        self.assertEqual(self.TE.getWikiPage(None), "No Timeline Avaliable")
+        self.assertIsInstance(self.TE.generateTimeline("Stanford University"), str)
+        self.assertEqual(self.TE.generateTimeline(""), "No Timeline Avaliable")
+        self.assertEqual(self.TE.generateTimeline(None), "No Timeline Avaliable")
 
     def test_timelineGenerationCheck(self):
-        self.assertTrue(self.TE.("Stanford University"), str)
+        self.assertTrue(self.TE.getWikiPage("Stanford University"), str)
         self.assertFalse(self.TE.getWikiPage(""), str)
         self.assertFalse(self.TE.getWikiPage(None), str)
 
 
 
 if __name__ == '__main__':
+    #os.system("python server.py")
     unittest.main()
